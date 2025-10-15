c719ace323cb5bc0ebe1067dc362c45667ea371b
Alek-Grujic <alek.grujic001@outlook.com>
2025-10-15 07:25:46 +0200

    Picking elements from the back with slice method

--- script.js
+++ script.js
@@ -80,7 +80,8 @@ let arr = ["a", "b", "c", "d", "e"];
 // SLICE
 
 // let sliced = arr.slice(0, 3);
-let sliced = arr.slice();
+// let sliced = arr.slice(); // duplicated array
+let sliced = arr.slice(-1); // picking elements from the back
 
 console.log(arr); // doesn't change
 console.log(sliced);
