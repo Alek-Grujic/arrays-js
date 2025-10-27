4819407c44b278f37fa92075f6a46e3cb650e670
Alek-Grujic <alek.grujic001@outlook.com>
2025-10-27 05:50:50 +0100

    feat: calculate average human age of adult dogs using map, filter, and reduce

--- script.js
+++ script.js
@@ -369,14 +369,46 @@ GOOD LUCK 😀
 
 // REDUCE
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-const balance = movements.reduce(function (acc, curr, i) {
-  return acc + curr;
-}, 0);
+// const balance = movements.reduce(function (acc, curr, i) {
+//   return acc + curr;
+// }, 0);
 
-console.log(balance);
+// console.log(balance);
 
-const maxValue = movements.reduce((acc, curr) => (acc > curr ? acc : curr));
+// const maxValue = movements.reduce((acc, curr) => (acc > curr ? acc : curr));
 
-console.log(maxValue);
+// console.log(maxValue);
+
+// Coding Challenge #2
+
+/* 
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
+
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+
+1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4.
+2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
+3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
+4. Run the function for both test datasets
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+*/
+
+function calcAverageHumanAge(ages) {
+  let humanAge = ages
+    .map((age) => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter((filteredAge) => filteredAge >= 18);
+  // .reduce((acc, curr) => acc + curr);
+  let reduced = humanAge.reduce((acc, curr) => acc + curr, 0);
+  let average;
+  if (humanAge.length !== 0) {
+    average = reduced / humanAge.length;
+    return average;
+  }
+}
+
+console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
