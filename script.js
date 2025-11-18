3187c2275a5275220937252c436fbb18a8e5e23d
Alek-Grujic <alek.grujic001@outlook.com>
2025-11-18 07:22:11 +0100

    feat: generate unique swimmingAdjacent activities by filtering swimming breeds and removing swimming activity

--- script.js
+++ script.js
@@ -776,4 +776,21 @@ console.log("------------");
 
 let uniqueActivities = [...new Set(allActivities)];
 
-console.log(uniqeActivities);
+console.log(uniqueActivities);
+
+console.log("------------");
+
+// 5. task
+//  Many dog breeds like to swim. What other activities do these dogs like? Store all the OTHER activities these breeds like to do, in a unique array called "swimmingAdjacent".
+
+let breedsLikeSwimming = breeds
+  .filter((str) => str.activities.includes("swimming"))
+  .flatMap((other) => other.activities);
+
+let swimmingAdjacent = [...new Set(breedsLikeSwimming)];
+
+let indexOfSwimming = [...swimmingAdjacent].indexOf("swimming");
+
+swimmingAdjacent.splice(indexOfSwimming, 1);
+
+console.log(swimmingAdjacent);
