ec7f7c236c32838ac0f7981d292465d72521903b
Alek-Grujic <alek.grujic001@outlook.com>
2025-11-22 11:07:14 +0100

    feat: implement title case conversion with exception handling

--- script.js
+++ script.js
@@ -1009,3 +1009,24 @@ const { deposits, withdrawals } = accounts
   );
 
 console.log(deposits, withdrawals);
+
+// 3.
+
+// this is a nice title -> This Is a Nice Title
+const convertTitleCase = function (title) {
+  const exceptions = ["a", "an", "the", "but", "or", "on", "in", "with", "and"];
+
+  const titleCase = title
+    .toLowerCase()
+    .split(" ")
+    .map((word) =>
+      exceptions.includes(word) ? word : word[0].toUpperCase() + word.slice(1)
+    )
+    .join(" ");
+
+  return titleCase;
+};
+
+console.log(convertTitleCase("this is a nice title"));
+console.log(convertTitleCase("this is a LONG title but not too long"));
+console.log(convertTitleCase("and here is another title with an EXAMPLE"));
